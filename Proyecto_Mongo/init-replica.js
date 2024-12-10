386dbfb4ff531697ff94a90090db492c9ba442cc
JAVP013 <joalvaldezpo@ittepic.edu.mx>
2024-12-09 19:52:01 -0700

    Mongo final

--- Proyecto_Mongo/init-replica.js
+++ Proyecto_Mongo/init-replica.js
@@ -1,13 +1,3 @@
-rs.initiate({
-_id: "replica01",
-members: [
-{ _id: 0, host: "mongo-primary:27017" },
-{ _id: 1, host: "mongo-secondary1:27017" },
-{ _id: 2, host: "mongo-secondary2:27017" },
-{ _id: 3, host: "mongo-secondary3:27017" }
-]
-})
-
 
 
 
@@ -20,6 +10,3 @@ rs.initiate({
     { _id: 3, host: "mongo-secondary3:27017", priority: 1 }
   ]
 })
-
-
-
